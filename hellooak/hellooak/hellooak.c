55fb1baa10f3b3fb6fec35964d7692e4b9940ef6
oakfire <oakfire@163.com>
2022-08-23 10:08:07 +0800

    feat: Add hellooak package.

---
+++ hellooak/hellooak/hellooak.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+ 
+int main(int argc, char**argv)
+{
+    printf("Hello oak! \nLife has become richer by the love that has been lost.\n\n");
+    return 0;
+}
